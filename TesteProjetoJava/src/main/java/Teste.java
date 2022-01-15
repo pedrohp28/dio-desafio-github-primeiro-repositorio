c5f14e0af502c147f95db4b360f045f4ba0c212f
pedrohp28 <pedrohp36@gmail.com>
2022-01-15 11:26:32 -0300

    Teste Projeto Java

---
+++ TesteProjetoJava/src/main/java/Teste.java
@@ -0,0 +1,16 @@
+
+public class Teste {
+
+
+    public static void main(String[] args) {
+        
+        int valor = 2, valor2 = 3, total;
+        
+        System.out.println("Teste projeto em Java");
+        
+        total = valor +valor2;
+        
+        System.out.println("A soma de " + valor + " + " + valor2 + " = "  + total);
+    }
+    
+}
